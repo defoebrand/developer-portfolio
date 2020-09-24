dde25c373bc7ad12a1ac15d772cfe7c172726357
Defoe.Brand <defoe.brand@gmail.com>
2020-09-24 16:06:50 +0300

    Set up side nav hover effects

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -2,4 +2,6 @@ class PortalController < ApplicationController
   # GET /portal
   # GET /portal.json
   def index; end
+
+  # def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :apps
   resources :websites
   resources :portal
+  get '/about', to: 'portal#about'
   root 'portal#index'
   # root 'index.html.erb'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
