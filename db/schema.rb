7a84fd2f6d7fafee49f975a72e4c43bb2f24ed88
Defoe.Brand <defoe.brand@gmail.com>
2020-10-13 18:07:01 +0300

    Adjust color scheme and add link for code view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_153834) do
+ActiveRecord::Schema.define(version: 2020_10_13_150219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_153834) do
     t.string "title"
     t.text "description"
     t.string "image"
+    t.string "code"
   end
 
   create_table "contact_form_submissions", force: :cascade do |t|
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_153834) do
     t.string "title"
     t.text "description"
     t.string "image"
+    t.string "code"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_153834) do
     t.string "title"
     t.text "description"
     t.string "image"
+    t.string "code"
   end
 
 end
