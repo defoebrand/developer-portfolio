c9c987a2bfeb3ff1fba1fa5d302ef28e95a8f8d7
Defoe.Brand <defoe.brand@gmail.com>
2020-10-14 19:26:43 +0300

    Update views to allow dynamic color of tech stacks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_153021) do
+ActiveRecord::Schema.define(version: 2020_10_14_161209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_153021) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "color"
   end
 
   create_table "stacks_websites", id: false, force: :cascade do |t|
