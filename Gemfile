6671748cb135769af5aa071bd81e055dd13090ae
defoebrand <defoe.brand@gmail.com>
2021-02-09 12:01:18 +0200

    Add appropriate linters

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
+gem 'react-rails'
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
@@ -31,6 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'hirb'
 end
 
 group :development do
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,50 +19,43 @@ require("channels")
 //= require popper
 //= require bootstrap-sprockets
 
-document.addEventListener("turbolinks:load", function() {
-
-  $(document).ready(function() {
-
-
-    $('.web-form').mouseover(function(event) {
+document.addEventListener('turbolinks:load', () => {
+  $(document).ready(() => {
+    $('.web-form').mouseover(event => {
       $('.title-change').text('coder');
       $('.interest-change').text('debug code');
     });
 
-    $('.employee-form').mouseover(function(event) {
+    $('.employee-form').mouseover(event => {
       $('.title-change').text('self-starter');
       $('.interest-change').text('solve problems');
     });
 
-    $('.tutor-form').mouseover(function(event) {
+    $('.tutor-form').mouseover(event => {
       $('.title-change').text('teacher');
       $('.interest-change').text('help others');
     });
 
-    $('.click-for-more-website').click(function(event) {
+    $('.click-for-more-website').click(event => {
       // $('.website-pop-out').toggleClass('show-pop-out');
       $('.website-pop-out').toggle();
       $('.employee-pop-out').hide();
       $('.tutor-pop-out').hide();
     });
 
-
-    $('.click-for-more-employee').click(function(event) {
+    $('.click-for-more-employee').click(event => {
       // $('.employee-pop-out').toggleClass('show-pop-out');
       $('.employee-pop-out').toggle();
       $('.website-pop-out').hide();
       $('.tutor-pop-out').hide();
     });
 
-
-    $('.click-for-more-tutor').click(function(event) {
+    $('.click-for-more-tutor').click(event => {
       // $('.tutor-pop-out').toggleClass('show-pop-out');
       $('.tutor-pop-out').toggle();
       $('.website-pop-out').hide();
       $('.employee-pop-out').hide();
     });
-
-
 
     // $('.up').click(function(event) {
     //   let top_value = $(".content").css("top");
@@ -110,8 +102,5 @@ document.addEventListener("turbolinks:load", function() {
     //     top: "0"
     //   });
     // });
-
-
   });
-
 });
