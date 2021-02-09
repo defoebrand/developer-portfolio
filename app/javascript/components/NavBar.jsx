5a52c38d6743fe5b21fe5354fc640ba6307a1aa6
defoebrand <defoe.brand@gmail.com>
2021-02-09 16:11:54 +0200

    NavBar and settings cog shift with screen ratio

--- app/javascript/components/NavBar.jsx
+++ app/javascript/components/NavBar.jsx
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import NavItem from './NavItem';
 import SiteItem from './SiteItem';
 
-const NavBar = ({ links }) => (
-  <nav className="d-flex flex-column col-sm justify-content-around align-items-center position-absolute">
+const horizDirection = 'd-flex col-sm justify-content-around align-items-center position-absolute topBar';
 
-    {links.sort((a, b) => a.order - b.order).map(link => (link.direction === 'internal'
-      ? (
-        <SiteItem
-          key={link.name}
-          name={link.name}
-          url={link.url}
-          icon={link.icon}
-        />
-      ) : (
-        <NavItem
-          key={link.name}
-          name={link.name}
-          url={link.url}
-          icon={link.icon}
-        />
-      )))}
+const vertDirection = 'd-flex flex-column col-sm justify-content-around align-items-center position-absolute';
 
-  </nav>
-);
+const NavBar = ({ links }) => {
+  const [direction, setDirection] = useState();
+
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  let movementTimer = null;
+
+  const RESET_TIMEOUT = 10;
+
+  const testDimensions = () => {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    testDimensions();
+  }, []);
+
+  window.addEventListener('resize', () => {
+    clearInterval(movementTimer);
+    movementTimer = setTimeout(testDimensions, RESET_TIMEOUT);
+  });
+
+  useEffect(() => {
+    const settings = document.querySelector('.signIn');
+    if (dimensions.width <= dimensions.height) {
+      setDirection(horizDirection);
+      settings.classList.remove('topRight');
+      settings.classList.add('bottomLeft');
+    } else {
+      setDirection(vertDirection);
+      settings.classList.remove('bottomLeft');
+      settings.classList.add('topRight');
+    }
+  }, [dimensions]);
+
+  return (
+    <nav className={direction}>
+
+      {links.sort((a, b) => a.order - b.order).map(link => (link.direction === 'internal'
+        ? (
+          <SiteItem
+            key={link.name}
+            name={link.name}
+            url={link.url}
+            icon={link.icon}
+          />
+        ) : (
+          <NavItem
+            key={link.name}
+            name={link.name}
+            url={link.url}
+            icon={link.icon}
+          />
+        )))}
+
+    </nav>
+  );
+};
 
 NavBar.propTypes = {
   links: PropTypes.arrayOf(
