9566591362e407b59f7f7a78f7ab3b4102374be9
defoebrand <defoe.brand@gmail.com>
2021-02-15 16:52:38 +0200

    Add smtp settings to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "developer_portfolio_production"
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'defoe.brand',
+    password: 'cpkrhqcuvyimtcln',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
