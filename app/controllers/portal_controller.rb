9e9fa00859fd6889f0792fd1c6d9e44656ee3e10
defoebrand <defoe.brand@gmail.com>
2021-02-17 19:18:04 +0200

    Further logging

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -160,7 +160,9 @@ class PortalController < ApplicationController
     request = Net::HTTP::Post.new(url)
     request['Content-Type'] = 'application/json'
     request['Authorization'] = 'Bearer 84a3583043afeb6745cf0b8f1e885f38b871d494b3d95e9260f4fa5235cd516c'
+    p 'hello'
     request.body = user_signed_in? ? current_user.room_token : params[:room_token]
+    p request.body
     response = http.request(request)
     token = response.read_body
 
