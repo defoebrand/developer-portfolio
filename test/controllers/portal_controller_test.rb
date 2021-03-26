ed85956ba9235df03a7a91ef7e96bccd8abaf881
Brandon <defoe.brand@gmail.com>
2021-03-26 16:30:27 +0200

    Reset all tests - add app model test

--- test/controllers/portal_controller_test.rb
+++ test/controllers/portal_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PortalControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get root_url
+    assert_response :success
+  end
 end
